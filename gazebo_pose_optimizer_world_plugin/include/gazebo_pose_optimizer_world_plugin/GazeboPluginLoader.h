c18220a6f55678c03caac807b52eca92b6b47749
hiro <hironoriyh@gmail.com>
2016-04-13 16:17:55 +0200

    gazebo world plugin initial.

---
+++ gazebo_pose_optimizer_world_plugin/include/gazebo_pose_optimizer_world_plugin/GazeboPluginLoader.h
@@ -0,0 +1,21 @@
+#ifndef GAZEBO_GAZEBOPLUGINLOADER_H
+#define GAZEBO_GAZEBOPLUGINLOADER_H
+
+#include <ros/ros.h>
+#include <gazebo/gazebo.hh>
+#include <gazebo/common/Plugin.hh>
+
+namespace gazebo
+{
+
+class GazeboPluginLoader : public WorldPlugin
+{
+public: 
+	//GazeboPluginLoader();
+	void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf);
+
+private:
+};
+
+}
+#endif  // GAZEBO_GAZEBOPLUGINLOADER_H
---
+++ gazebo_pose_optimizer_world_plugin/src/GazeboPluginLoader.cpp
@@ -0,0 +1,65 @@
+#include <gazebo_pose_optimizer_world_plugin/GazeboPluginLoader.h>
+
+#include <gazebo/physics/World.hh>
+#include <gazebo/physics/PhysicsIface.hh>
+
+#include <pluginlib/class_list_macros.h>
+
+using gazebo::GazeboPluginLoader;
+
+
+void GazeboPluginLoader::Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
+{
+	//ros::init();
+	ros::NodeHandle node_handle("~");
+	ros::AsyncSpinner spinner(1);
+	spinner.start();
+	ros::waitForShutdown();
+}
+
+
+//
+//void GazeboPluginLoader::onWorldCreate(){
+//    ROS_INFO("On-World-Create: Reading list of Gazebo world plugins to be loaded from parameters...");
+//
+//    ros::NodeHandle node("/gazebo_state_plugins");
+//
+//    ROS_INFO_STREAM("Reading parameters from namespace "<<node.getNamespace());
+//
+//    std::string world_name;
+//    node.getParam("world_name", world_name);
+//    ROS_INFO_STREAM("Loading plugins for world '"<<world_name<<"'");
+//
+//    physics::WorldPtr world = gazebo::physics::get_world(world_name);
+//    if (!world)
+//    {
+//        ROS_ERROR("Could not obtain pointer to world from system plugin, so won't be able to load plugins");
+//        return;
+//    }
+//
+//    XmlRpc::XmlRpcValue world_plugins;
+//    node.getParam("world_plugins", world_plugins);
+//    if (world_plugins.getType() != XmlRpc::XmlRpcValue::TypeArray)
+//    {
+//      ROS_ERROR("Parameter world_plugins should be specified as an array. Gazebo world plugins won't be loaded.");
+//      return;
+//    }
+//
+//     get the list of names
+//    for (int i = 0 ; i < world_plugins.size() ; ++i)
+//    {
+//      if (!world_plugins[i].hasMember("name") || !world_plugins[i].hasMember("file"))
+//      {
+//            ROS_ERROR("World plugin parameter specification should have 'name' and 'file'. Gazebo world plugins won't be loaded.");
+//            continue;
+//      }
+//      std::string name=world_plugins[i]["name"];
+//      std::string file=world_plugins[i]["file"];
+//      sdf::ElementPtr _sdf;  // will be initialized to NULL
+//      world->LoadPlugin(file, name, _sdf);
+//    }
+//
+//    ROS_INFO("Eligible plugins loaded.");
+//}
+
+GZ_REGISTER_WORLD_PLUGIN(GazeboPluginLoader)
