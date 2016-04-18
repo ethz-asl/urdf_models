103205c4202f1a171fbf52594004e1bee7154b34
hiro <hironoriyh@gmail.com>
2016-04-18 13:47:24 +0200

    plugin is properly built.

--- gazebo_pose_optimizer_world_plugin/include/gazebo_pose_optimizer_world_plugin/GazeboPluginLoader.h
+++ gazebo_pose_optimizer_world_plugin/include/gazebo_pose_optimizer_world_plugin/GazeboPluginLoader.h
@@ -5,16 +5,49 @@
 #include <gazebo/gazebo.hh>
 #include <gazebo/common/Plugin.hh>
 
+#include <object_msgs/Object.h>
+#include <object_msgs/ObjectInfo.h>
+#include <object_msgs/ObjectInfoRequest.h>
+#include <object_msgs/ObjectInfoResponse.h>
+
+#include <shape_msgs/SolidPrimitive.h>
+#include <geometry_msgs/Pose.h>
+
 namespace gazebo
 {
 
 class GazeboPluginLoader : public WorldPlugin
 {
 public: 
+  typedef object_msgs::Object ObjectMsg;
+  typedef object_msgs::ObjectInfo ObjectInfoMsg;
+
 	//GazeboPluginLoader();
+  GazeboPluginLoader();
 	void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf);
 
 private:
+  bool requestObject(object_msgs::ObjectInfo::Request  &req, object_msgs::ObjectInfo::Response &res);
+  void advertEvent(const ros::TimerEvent& e);
+  void onWorldUpdate();
+
+
+private:
+  bool PUBLISH_OBJECTS;
+  std::string WORLD_OBJECTS_TOPIC;
+  std::string REQUEST_OBJECTS_TOPIC;
+  std::string ROOT_FRAME_ID;
+
+  physics::WorldPtr world;
+
+  event::ConnectionPtr update_connection;
+  ros::Publisher object_pub;
+  ros::ServiceServer request_object_srv;
+
+  ros::Timer publishTimer;
+
+  std::vector<ObjectMsg> lastGeneratedObjects;
+  bool reGenerateObjects;
 
 };
 
--- gazebo_pose_optimizer_world_plugin/src/GazeboPluginLoader.cpp
+++ gazebo_pose_optimizer_world_plugin/src/GazeboPluginLoader.cpp
@@ -1,15 +1,44 @@
 #include <gazebo_pose_optimizer_world_plugin/GazeboPluginLoader.h>
 
+#include <gazebo/physics/Link.hh>
+#include <gazebo/physics/BoxShape.hh>
+#include <gazebo/physics/SphereShape.hh>
+#include <gazebo/physics/CylinderShape.hh>
+#include <gazebo/physics/Collision.hh>
+#include <gazebo/physics/MeshShape.hh>
 #include <gazebo/physics/World.hh>
-#include <gazebo/physics/PhysicsIface.hh>
-#include <vector>
-#include <pluginlib/class_list_macros.h>
+
+#include <gazebo/common/Plugin.hh>
+#include <gazebo/common/Events.hh>
+#include <gazebo/common/common.hh>
+
+#include <algorithm>
+#include <assert.h>
+#include <boost/bind.hpp>
+#include <boost/thread/mutex.hpp>
+
+
+#define DEFAULT_PUBLISH_OBJECTS false
+#define DEFAULT_WORLD_OBJECTS_TOPIC "world/objects"
+#define DEFAULT_REQUEST_OBJECTS_TOPIC "world/request_object"
+#define DEFAULT_ROOT_FRAME_ID "world"
+
+//publishing rate
+#define UPDATE_RATE 5
+
+#define OBJECT_QUEUE_SIZE 100
 
 using gazebo::GazeboPluginLoader;
 
+GazeboPluginLoader::GazeboPluginLoader() :
+    WorldPlugin(){
+    //reGenerateObjects=true;
+}
 
 void GazeboPluginLoader::Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
 {
+
+  ros::NodeHandle node("/pose_generator");
 
     if (!ros::isInitialized())
     {
@@ -18,19 +47,71 @@ void GazeboPluginLoader::Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
       return;
     }
 
+    node.param<bool>("publish_world_objects", PUBLISH_OBJECTS, DEFAULT_PUBLISH_OBJECTS);
+    ROS_INFO("GazeboObjInfo: Got objects publish flag: <%i>", PUBLISH_OBJECTS);
 
-//	ros::NodeHandle node_handle("~");
-//	ros::AsyncSpinner spinner(1);
-//	spinner.start();
-//	ros::waitForShutdown();
+    node.param<std::string>("world_objects_topic", WORLD_OBJECTS_TOPIC, DEFAULT_WORLD_OBJECTS_TOPIC);
+    ROS_INFO("GazeboObjInfo: Got objects topic name: <%s>", WORLD_OBJECTS_TOPIC.c_str());
 
+    node.param<std::string>("request_object_service", REQUEST_OBJECTS_TOPIC, DEFAULT_REQUEST_OBJECTS_TOPIC);
+    ROS_INFO("GazeboObjInfo: Got objects topic name: <%s>", REQUEST_OBJECTS_TOPIC.c_str());
 
-	//std::vector<gazebo::physics::Model> models_ = _world->GetModels();
+    node.param<std::string>("objects_frame_id", ROOT_FRAME_ID, DEFAULT_ROOT_FRAME_ID);
+    ROS_INFO("GazeboObjInfo: Got objects frame id: <%s>", ROOT_FRAME_ID.c_str());
+
+    world=_world;
+
+    if (PUBLISH_OBJECTS){
+        update_connection = event::Events::ConnectWorldUpdateBegin(boost::bind(&GazeboPluginLoader::onWorldUpdate, this));
+        object_pub = node.advertise<GazeboPluginLoader::ObjectMsg>(WORLD_OBJECTS_TOPIC, OBJECT_QUEUE_SIZE);
+    }
+
+    ros::Rate rate(UPDATE_RATE);
 
     ROS_INFO("Hello World!");
+    world=_world;
+
+    request_object_srv = node.advertiseService(REQUEST_OBJECTS_TOPIC, &GazeboPluginLoader::requestObject,this);
+
 	//ROS_INFO_STREAM(models_);
 }
 
+bool GazeboPluginLoader::requestObject(object_msgs::ObjectInfo::Request &req, object_msgs::ObjectInfo::Response &res) {
+
+    std::string modelName=req.name;
+    physics::ModelPtr model=world->GetModel(modelName);
+
+    if (!model.get()) {
+        // ROS_ERROR("Model %s not found",modelName.c_str());
+        res.success=false;
+        res.error_code=object_msgs::ObjectInfo::Response::OBJECT_NOT_FOUND;
+        return true;
+    }
+
+    res.error_code=object_msgs::ObjectInfo::Response::NO_ERROR;
+    res.success=true;
+   // res.object=createBoundingBoxObject(model,req.get_geometry);
+    //ROS_INFO("Received service request for object info!");
+    return true;
+}
+
+void GazeboPluginLoader::onWorldUpdate() {
+    if (object_pub.getNumSubscribers()==0) return;
+//
+//    if (!reGenerateObjects) return;
+//
+//    physics::Model_V models=world->GetModels();
+//    physics::Model_V::iterator m_it;
+//
+//    bool send_shape=false;
+//    for (m_it=models.begin(); m_it!=models.end(); ++m_it) {
+//        //ROS_INFO_STREAM("Have model "<<(*m_it)->GetName());
+//        GazeboObjectInfo::ObjectMsg obj=createBoundingBoxObject(*m_it,send_shape);
+//        lastGeneratedObjects.push_back(obj);
+//    }
+//
+//    reGenerateObjects=false;
+}
 
 
 //
